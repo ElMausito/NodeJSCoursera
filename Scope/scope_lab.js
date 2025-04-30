b39922f2e0021c40e471a0cdb0ca9f7db34e78d8
ElMausito <elmausx@gmail.com>
2025-04-30 00:17:22 -0400

    Scope global local variables

---
+++ Scope/scope_lab.js
@@ -0,0 +1,17 @@
+//Ámbito Global
+//function show(){
+var globalvar= "Soy una variable global";
+let globalLet= "Tambien soy global pero con ambito de LET";
+const globalConst="soy una constante global";
+console.log(globalvar);
+console.log(globalLet);
+console.log(globalConst);
+//}
+//show();
+{
+    //Ambito de bloque
+
+    var blockVar= "Soy un var con ambito de bloque";
+    let blockLet="Soy un let con ambito de bloque";
+    const blockConst="Soy un const con ámbito de bloque";
+    }
